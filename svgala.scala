6c434342f5227deea6906e7bee864c345eae22df
toma63 <t_arns@yahoo.com>
2016-12-03 20:58:53 -0800

    separate handling of presentation attributes

--- svgala.scala
+++ svgala.scala
@@ -1,5 +1,5 @@
 // create an svg display object
-package svgala
+//package svgala
 
 import scala.collection.mutable.ListBuffer
 import java.io._
@@ -11,18 +11,38 @@ class SVGala(val width: Int, val height: Int) {
 
   // utilities
 
+  // get the attribute name from an attribute assignment string
+  def getAttrName(attr: String): String = attr.split('=')(0)
+
   // move origin to lower left by adjusting y coordinate
   def flipY(y: Int, oHeight: Int): Int = height - (y + oHeight)
+
+  // uniform DRY handling of presentation attributes
+  // takes the element specific part of the definition and combines it with
+  // any presentation attributes added as an array of Strings.
+  // Completes the svg tag and returns the entire string to add to the document.
+  // apply defauls and filter out override of defaults.
+  def addPresentationAttr(element: String, attr: Seq[String], defaultAttr: Array[String]): String = {
+    val attrNames = attr.map((a: String) => getAttrName(a)) // just attr name
+    val filteredDefaultAttr = defaultAttr.filter((da: String) => !attrNames.contains(getAttrName(da)))
+    val finalAttr = attr ++ filteredDefaultAttr
+    element + finalAttr.reduce((a: String, b: String) => a + " " + b) + "/>"
+  }
+
+  // methods which create object tags as strings
+
+  // Create an object tag for a rectangle
+  def makeRect(x: Int, y: Int, width: Int, height: Int, attr: Seq[String]): String = {
+    val svg1 = s"""<rect x="$x" y="${flipY(y, height)}" width="$width" height="$height" """
+    val defaultAttr = Array("""fill="0X0000FF"""", """stroke-width="1"""", """stroke="0x000000"""")
+    addPresentationAttr(svg1, attr, defaultAttr)
+  }
 
   // methods which add objects to the display
 
   // add a rectangle
-  def addRect(x: Int, y: Int, width: Int, height: Int,
-    fill: String = "0X0000FF", strokeWidth: String = "1", stroke: String = "0x000000") {
-
-    val svg1 = s"""<rect x="$x" y="${flipY(y, height)}" width="$width" height="$height" """
-    val svg2 = svg1 + s"""fill="$fill" stroke-width="$strokeWidth" stroke="$stroke"/>"""
-    displayList += svg2
+  def addRect(x: Int, y: Int, width: Int, height: Int, attr: String*) {
+    displayList += makeRect(x, y, width, height, attr)
   }
 
   // output
